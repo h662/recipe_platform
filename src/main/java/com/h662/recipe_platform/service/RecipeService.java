4b8d985c5d7f49d58e3ae2f8c81be408b74f026a
h662 <h662hong@gmail.com>
2025-07-17 13:35:53 +0900

    addIngredient

--- src/main/java/com/h662/recipe_platform/service/RecipeService.java
+++ src/main/java/com/h662/recipe_platform/service/RecipeService.java
@@ -5,6 +5,8 @@ import com.h662.recipe_platform.dto.RecipeDto;
 import com.h662.recipe_platform.dto.RecipeResponseDto;
 import com.h662.recipe_platform.model.Ingredient;
 import com.h662.recipe_platform.model.Recipe;
+import com.h662.recipe_platform.model.RecipeIngredient;
+import com.h662.recipe_platform.model.RecipeIngredientId;
 import com.h662.recipe_platform.repository.IngredientRepository;
 import com.h662.recipe_platform.repository.RecipeIngredientRepository;
 import com.h662.recipe_platform.repository.RecipeRepository;
@@ -35,5 +37,20 @@ public class RecipeService {
                 .orElseThrow(() -> new NoSuchElementException("레시피를 찾을 수 없습니다."));
         Ingredient ingredient = ingredientRepository.findById(dto.getIngredientId())
                 .orElseThrow(() -> new NoSuchElementException("재료를 찾을 수 없습니다."));
+
+        RecipeIngredientId id = new RecipeIngredientId(recipeId, ingredient.getId());
+        if(recipeIngredientRepository.existsById(id)) {
+            throw new IllegalStateException("이미 추가된 재료입니다.");
+        }
+
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.setId(id);
+        recipeIngredient.setRecipe(recipe);
+        recipeIngredient.setIngredient(ingredient);
+        recipeIngredient.setQuantity(dto.getQuantity());
+
+        recipe.getRecipeIngredients().add(recipeIngredient);
+
+        recipeRepository.save(recipe);
     }
 }
